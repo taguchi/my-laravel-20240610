5a1a20f9d5f45b7755826830c0d258564dae4349
Gen Taguchi <taguchi@users.noreply.github.com>
2024-06-10 16:36:15 +0900

    Initial commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo ".env" >> .gitignore
+composer install
+php artisan key:generate
+touch database/database.sqlite
+php artisan migrate
+sudo apt-get update
+sudo apt-get install sqlite3
+
+# invalidate xdebug
+sudo chmod 777 /usr/local/etc/php/conf.d/xdebug.ini
+cp /usr/local/etc/php/conf.d/xdebug.ini /tmp/xdebug.ini
+sed -i 's/^/;/g' /tmp/xdebug.ini
+cp -f /tmp/xdebug.ini /usr/local/etc/php/conf.d/xdebug.ini
+rm /tmp/xdebug.ini
+
+# start server
+php artisan serve --port=8573
